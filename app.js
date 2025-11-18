cedda582fad6254ccfa56e85268fc2cee8c39d65
Felipe Bogaz Ferreira <163551470+SkolpionMK@users.noreply.github.com>
2025-11-18 14:13:37 -0400

    Add route to respond with name

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+    res.send('“Meu nome é Felipe Bogaz Ferreira” 🤩')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
